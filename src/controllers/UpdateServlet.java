ac17beefb055002d6e1db586aa43c57aa1fb7d2a
tonnura <masaki0224@icloud.com>
2024-11-04 13:13:02 +0900

    バリデーションを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
+import models.validators.MessageValidator;
 import utils.DBUtil;
-
 /**
  * Servlet implementation class UpdateServlet
  */
@@ -50,6 +52,19 @@ public class UpdateServlet extends HttpServlet {
 			Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 			m.setUpdated_at(currentTime);
 
+			List<String> errors = MessageValidator.validate(m);
+			if(errors.size() > 0) {
+				em.close();
+
+				request.setAttribute("_token", request.getSession().getId());
+				request.setAttribute("message", m);
+				request.setAttribute("errors", errors);
+
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+				rd.forward(request, response);
+
+			} else {
+
 			em.getTransaction().begin();
 			em.getTransaction().commit();
 			request.getSession().setAttribute("flush", "登録が完了しました。");
@@ -59,7 +74,7 @@ public class UpdateServlet extends HttpServlet {
 			request.getSession().removeAttribute("message_id");
 
 			response.sendRedirect(request.getContextPath() + "/index");
-
+			}
 
 	}
 }
